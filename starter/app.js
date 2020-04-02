bfcf6fa92d693b3ab691bbade7a96a05146ac9c3
anson1014 <anson1014@hotmail.com>
2020-04-01 17:43:32 -0700

    created the modules for the module pattern

---
+++ starter/app.js
@@ -0,0 +1,28 @@
+var budgetController = (function() {
+
+
+})();
+
+
+var UIController = (function() {
+
+})();
+
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+
+    var ctrlAddItem = function() {
+        console.log('yes');
+
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    })
+
+})(budgetController, UIController);
